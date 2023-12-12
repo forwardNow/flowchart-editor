4840fe68bd0326f132735da7bdd74cb4be86b963
吴钦飞 <wuqinfei_csdn@163.com>
2023-12-12 11:36:29 +0800

    build copy: init

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 const { defineConfig } = require('@vue/cli-service');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const IS_PROD = process.env.NODE_ENV === 'production';
 
 module.exports = defineConfig({
   transpileDependencies: true,
   lintOnSave: false,
 
   configureWebpack: {
+    plugins: [
+      IS_PROD ? new CopyPlugin({
+        patterns: [
+          'public/demo',
+        ],
+      }) : null,
+    ].filter(Boolean),
   },
 });
