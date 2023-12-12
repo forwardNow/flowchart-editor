530e720ab08fa66ee144aec3a9b6f437b404cd03
吴钦飞 <wuqinfei_csdn@163.com>
2023-12-12 19:10:17 +0800

    build babel: 可选链式操作符 polyfill

--- babel.config.js
+++ babel.config.js
@@ -2,4 +2,8 @@ module.exports = {
   presets: [
     '@vue/cli-plugin-babel/preset',
   ],
+  plugins: [
+    // 放在数组的最后一个位置，在其他插件执行完后再进行代码转换
+    '@babel/plugin-proposal-optional-chaining',
+  ],
 };
