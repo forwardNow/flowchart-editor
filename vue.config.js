0d8764c19ebe3f2d998da5c2113b273b798dec0f
吴钦飞 <wuqinfei_csdn@163.com>
2023-12-18 15:28:58 +0800

    build vue.config.js: 添加 dist-zip 功能

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
+const path = require('path');
 const { defineConfig } = require('@vue/cli-service');
+const ZipWebpackPlugin = require('zip-webpack-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
+const package = require('./package.json');
 
 const IS_PROD = process.env.NODE_ENV === 'production';
 
@@ -8,11 +11,24 @@ module.exports = defineConfig({
   lintOnSave: false,
 
   configureWebpack: {
+    // mode: IS_PROD ? 'production' : 'development',
+    // devtool: IS_PROD ? 'source-map' : 'cheap-module-source-map',
+
     plugins: [
       IS_PROD ? new CopyPlugin({
         patterns: [
           { from: 'public/demo', to: 'demo' },
+          { from: 'types', to: 'types' },
         ],
+      }) : null,
+
+      IS_PROD ? new ZipWebpackPlugin({
+        path: path.resolve(__dirname, 'dist-zip'),
+
+        filename: (function getFilename() {
+          const { name, version } = package;
+          return `${name}.${version}`;
+        }()),
       }) : null,
     ].filter(Boolean),
   },
