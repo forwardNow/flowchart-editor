3515df8d077e1a46c25b109e5905da0fc393f2c4
吴钦飞 <wuqinfei_csdn@163.com>
2024-02-22 10:55:08 +0800

    refactor ToolButtons: encapsulate drop-down menu

--- src/components/toolbox/ToolButtons.vue
+++ src/components/toolbox/ToolButtons.vue
@@ -1,106 +1,50 @@
 <template>
+  <!-- eslint-disable vue/singleline-html-element-content-newline  -->
   <div class="fc-tool-buttons fc-toolbox-list">
-    <div class="fc-toolbox-item">
+    <FcDropdown class="fc-toolbox-item">
       <span>文件</span>
-      <div class="fc-menu">
-        <div class="fc-menu-item" title="保存到 localStorage" @click="save">
-          <div class="fc-menu-item-inner">
-            <div class="fc-menu-item-icon fc-icon-save" />
-            <div class="fc-menu-item-text">
-              保存
-            </div>
-            <div class="fc-menu-item-hotkey">
-              Ctrl + S
-            </div>
-          </div>
-        </div>
+      <template #dropdown>
+        <FcDropdownMenu>
+          <FcDropdownMenuItem
+            icon="fc-icon-save" label="保存" hotkey="Ctrl + S"
+            title="保存到 localStorage" @click="save"
+          />
 
-        <div class="fc-menu-item-divider" />
-        <label class="fc-menu-item" title="导入流程图配置文件">
-          <span class="fc-menu-item-inner">
-            <span class="fc-menu-item-icon fc-icon-import" />
-            <span class="fc-menu-item-text">
-              <input v-show="false" type="file" @change="importConfigFile($event)">
+          <FcDropdownMenuItem divided />
+
+          <FcDropdownMenuItem
+            icon="fc-icon-import" label="导入流程图配置文件"
+            title="导入流程图配置文件" @click="$refs.fileInput.click()"
+          >
+            <template #label>
+              <input v-show="false" ref="fileInput" type="file" @change="importConfigFile($event)">
               导入
-            </span>
-          </span>
-        </label>
-        <div class="fc-menu-item" title="导出流程图配置文件" @click="downloadConfigFile">
-          <div class="fc-menu-item-inner">
-            <div class="fc-menu-item-icon fc-icon-download" />
-            <div class="fc-menu-item-text">
-              导出
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="fc-toolbox-item">
-      <span>编辑</span>
-      <div class="fc-menu">
-        <div class="fc-menu-item" title="删除节点或连线" @click="remove">
-          <div class="fc-menu-item-inner">
-            <div class="fc-menu-item-icon fc-icon-delete" />
-            <div class="fc-menu-item-text">
-              删除
-            </div>
-            <div class="fc-menu-item-hotkey">
-              Delete
-            </div>
-          </div>
-        </div>
+            </template>
+          </FcDropdownMenuItem>
 
-        <div class="fc-menu-item" title="重置画布缩放和偏移" @click="resetSetting">
-          <div class="fc-menu-item-inner">
-            <div class="fc-menu-item-icon fc-icon-reset" />
-            <div class="fc-menu-item-text">
-              重置
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <template v-if="false">
-      <div
-        class="fc-toolbox-item"
-        title="保存到 localStorage"
-        @click="save"
-      >
-        保存
-      </div>
-      <div
-        class="fc-toolbox-item"
-        title="删除节点或连线"
-        @click="remove"
-      >
-        删除
-      </div>
-      <label
-        class="fc-toolbox-item"
-        title="导入流程图配置文件"
-      >
-        <input
-          v-show="false"
-          type="file"
-          @change="importConfigFile($event)"
-        >
-        导入
-      </label>
-      <div
-        class="fc-toolbox-item"
-        title="导出流程图配置文件"
-        @click="downloadConfigFile"
-      >
-        导出
-      </div>
-      <div
-        class="fc-toolbox-item"
-        title="重置画布缩放和偏移"
-        @click="resetSetting"
-      >
-        重置
-      </div>
-    </template>
+          <FcDropdownMenuItem
+            icon="fc-icon-download" label="导出"
+            title="导出流程图配置文件" @click="downloadConfigFile"
+          />
+        </FcDropdownMenu>
+      </template>
+    </FcDropdown>
+
+    <FcDropdown class="fc-toolbox-item">
+      <span>编辑</span>
+      <template #dropdown>
+        <FcDropdownMenu>
+          <FcDropdownMenuItem
+            icon="fc-icon-delete" label="删除" hotkey="Delete"
+            title="删除节点或连线" @click="remove"
+          />
+          <FcDropdownMenuItem
+            icon="fc-icon-reset" label="重置"
+            title="重置画布缩放和偏移" @click="resetSetting"
+          />
+        </FcDropdownMenu>
+      </template>
+    </FcDropdown>
   </div>
 </template>
 <script>
@@ -111,8 +55,17 @@ import {
 } from '@/commons/configs/constants';
 import { showAlert, showConfirm, showSuccessToast } from '@/commons/utils/popup';
 import { downloadPlainFile } from '@/commons/utils/download';
+import FcDropdownMenu from '@/components/toolbox/components/FcDropdownMenu.vue';
+import FcDropdownMenuItem from '@/components/toolbox/components/FcDropdownMenuItem.vue';
+import FcDropdown from '@/components/toolbox/components/FcDropdown.vue';
 
 export default {
+  components: {
+    FcDropdown,
+    FcDropdownMenu,
+    FcDropdownMenuItem,
+  },
+
   inject: ['toolboxRef', 'flowChartRef'],
 
   methods: {
@@ -206,78 +159,6 @@ export default {
 </script>
 <style lang="scss">
 .fc-tool-buttons {
-  .fc-toolbox-item {
-    position: relative;
-
-    &:hover,
-    &.active {
-      .fc-menu {
-        display: block;
-      }
-    }
-  }
-
-  .fc-menu {
-    display: none;
-    position: absolute;
-    top: 100%;
-    left: 0;
-
-    padding-top: 4px;
-    padding-bottom: 2px;
-
-    width: 180px;
-
-    background: #fff;
-    border: #e9edf2;
-    box-shadow: 0 4px 10px 0 rgba(0,0,0,.1);
-    border-radius: 4px;
-    color: #212930;
-    font-size: 13px;
-  }
-
-  .fc-menu-item {
-    display: block;
-    padding: 0 4px;
-    margin-bottom: 2px;
-  }
-
-  .fc-menu-item-inner {
-    display: flex;
-    align-items: center;
-    justify-content: space-between;
-
-    padding-right: 8px;
-    height: 32px;
-    line-height: 32px;
-    border-radius: 4px;
-    cursor: pointer;
-
-    &:hover {
-      background-color: #f3f5f9;
-    }
-  }
-
-  .fc-menu-item-icon {
-    width: 32px;
-    height: 32px;
-    background: no-repeat center center;
-    background-size: 60% 60%;
-  }
-  .fc-menu-item-text {
-    flex: 1;
-  }
-  .fc-menu-item-hotkey {
-    padding-left: 32px;
-    color: #a6b9cd;
-    font-size: 12px;
-  }
-
-  .fc-menu-item-divider {
-    height: 0;
-    border-top: 1px solid #e9edf2;
-    margin: 4px;
-  }
 
   .fc-icon-save {
     background-image: url(@/commons/styles/images/icon_save.svg);
---
+++ src/components/toolbox/components/FcDropdown.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="fc-dropdown">
+    <slot />
+    <slot name="dropdown" />
+  </div>
+</template>
+<script>
+export default {
+
+};
+</script>
+<style lang="scss">
+.fc-dropdown {
+  position: relative;
+
+  &:hover,
+  &.active {
+    .fc-dropdown-menu {
+      display: block;
+    }
+  }
+}
+</style>
